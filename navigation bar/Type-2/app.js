9e69972402a34a45fa59765271148e3be311d7d2
ankit soni <ankisoni453@gmail.com>
2020-06-25 23:56:08 +0530

    adding more types of navigation bar

---
+++ navigation bar/Type-2/app.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    $('a.target-burger').click(function (e) {
+        $('div.container, nav.main-nav, a.target-burger').toggleClass('toggled');
+        e.preventDefault();
+    });
+});
